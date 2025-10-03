fdd485ca3beb1f3b1f9b26ffa270bd8138b78394
Asegid-Yabi <87968170+Asegid-Yabi@users.noreply.github.com>
2025-10-03 22:57:42 +0300

    test: break app to test CI (#2)

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-const os = require('os');
-console.log("=== DevOps Demo App ===");
-console.log("OS Platform:", os.platform());
-console.log("CPU Count:", os.cpus().length);
-console.log("Free Memo:", Math.round(os.freemem() / 1024 / 1024) + " MB");
+console.log(brokenVariable);
